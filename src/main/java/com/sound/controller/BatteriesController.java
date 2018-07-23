ae37e759dab89f9ed65872db9f95430f960ea8ba
liulin <liulindatoucong@163.com>
2018-07-23 17:53:17 +0800

    继续开发

--- src/main/java/com/sound/controller/BatteriesController.java
+++ src/main/java/com/sound/controller/BatteriesController.java
@@ -1,9 +1,16 @@
 package com.sound.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.sound.model.ResponseMessageM;
 
 @Controller
 @RequestMapping(path = "/batteries")
@@ -13,5 +20,15 @@ public class BatteriesController {
 	public ModelAndView accountIndex() {
 		ModelAndView mav = new ModelAndView("batteries/index");
 		return mav;
+	}
+	
+	@PostMapping(path = "/fileload")
+	public ResponseMessageM dealWithBatteriesInfo(HttpServletRequest request)
+	{
+		 MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+		 MultipartFile file = multipartRequest.getFile("excelfile");
+		 
+		return null;
+		
 	}
 }
